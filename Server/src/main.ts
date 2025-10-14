7e652f89e3ec59d1d81abeb2842b55b8feff077a
Mohamed Osama <141563962+mohamed-osamaaa@users.noreply.github.com>
2025-10-14 10:58:02 +0300

    fix bug in production

--- Server/src/main.ts
+++ Server/src/main.ts
@@ -13,6 +13,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.setGlobalPrefix('api');
+
   app.enableCors({
     origin: '*',
     // origin: 'http://localhost:5173',
